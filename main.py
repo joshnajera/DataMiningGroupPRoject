95f3d27f3cbea78981ac98fe6101cefb28a21fba
unknown <switch620@gmail.com>
2017-12-07 12:14:26 -0800

    Updated

--- main.py
+++ main.py
@@ -8,10 +8,15 @@ from sklearn.cross_validation import train_test_split
 from sklearn.naive_bayes import MultinomialNB, BernoulliNB, GaussianNB
 import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
 
-FILE = 'train.csv'
-df = pd.read_csv(FILE)
-print(df.head())
+TRAIN_FILE = 'train.csv'
+df = pd.read_csv(TRAIN_FILE)
+
+KAGGLE_TEST_FILE = 'test.csv'
+kaggle_test_df = pd.read_csv(KAGGLE_TEST_FILE)
+
+# print(df.head())
 # print(df[df.author =='EAP'].head())
 
 df.loc[df['author']=='EAP', 'author'] = 0
@@ -19,71 +24,76 @@ df.loc[df['author']=='HPL', 'author'] = 1
 df.loc[df['author']=='MWS', 'author'] = 2
 
 X = df['text']
+X_kaggle = kaggle_test_df['text']
 y = df['author'].astype('int')
 
 # Classifier
-mnb = MultinomialNB(fit_prior=True, class_prior=None)
+# mnb = MultinomialNB(fit_prior=True, class_prior=None)
 # bnb = BernoulliNB(alpha=1.0,binarize=.0,fit_prior=True,class_prior=None)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = .30, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = .01, random_state=0)
 
-#### Normal Vectorizer Test ####
-cv = CountVectorizer()
-# X_train = cv.fit_transform(X_train)
-# X_test = cv.transform(X_test)
-# print(cv.inverse_transform(X_test))
 
 #### TFIDF Vectorizer Test ####
-tfidf = TfidfVectorizer(stop_words=None)
-X_train = tfidf.fit_transform(X_train)
-X_test = tfidf.transform(X_test)
-# print(tfidf.inverse_transform(X_test))
+tfidf = TfidfVectorizer(stop_words='english')
+# X_train = tfidf.fit_transform(X_train)
+X = tfidf.fit_transform(X)
+# X_test = tfidf.transform(X_test)
+X_kaggle = tfidf.transform(X_kaggle)
 
-# print(y_test)
 
-# mnb.fit(X_train, y_train)
-mnb_ovr = OneVsRestClassifier(mnb, n_jobs=1).fit(X_train, y_train)
-print("mnb trained")
-# pred = mnb.predict(X_test)
-# print(pred)
+
+ ''' Grid Search '''
+clf = MultinomialNB(fit_prior=True, alpha=.01)
+# clf.fit(X_train, y_train)
+# print("MNB Solo score: {}".format(clf.score(X_test, y_test)))
+
+alpha_range = [i for i in np.arange(0.000,1.0,.015)]
+prior = [True, False]
+grid_clf = GridSearchCV(clf, {'alpha': alpha_range},scoring='accuracy', n_jobs=1, cv=100)
+grid_clf.fit(X, y)
+# print(grid_clf.grid_scores_)
+mean_scores = [result.mean_validation_score for result in grid_clf.grid_scores_]
+plt.plot(alpha_range, mean_scores)
+plt.xlabel('Alpha')
+plt.ylabel('Mean Score (CV=100)')
+plt.show()
+# print(mean_scores)
+print(grid_clf.best_score_, grid_clf.best_params_)
+
+
+
+''' One Vs Rest Classification from Grid Search Result '''
+mnb_ovr = OneVsRestClassifier(grid_clf.best_estimator_, n_jobs=1)
+# print("MNB OVR Score: {}".format(mnb_ovr.score(X_test, y_test)))
+mnb_ovr.fit(X, y)
+kaggle_results = mnb_ovr.predict_proba(X_kaggle)
+print(kaggle_results)
+
+ids = kaggle_test_df['id']
+predict_data = pd.DataFrame(kaggle_results,  columns=['EAP', 'HPL', 'MWS'])
+conc = pd.concat([ids, predict_data], axis=1)
+conc.to_csv('mySubmission.csv', index=False)
+
+
+''' Multi Layer Perceptron Tests 
+
 # mlp = MLPClassifier(hidden_layer_sizes=(30,15),activation="logistic",solver='adam',alpha=0.0001,batch_size='auto',learning_rate="constant",learning_rate_init=0.001,\
 #     power_t=0.5, max_iter=25, shuffle=True, random_state=None, tol=1e-4, verbose=True, warm_start=False, momentum=0.9, nesterovs_momentum=True, \
 #     early_stopping=False, validation_fraction=0.1, beta_1=0.9, beta_2=0.999, epsilon=1e-8)
-mlp = MLPClassifier(hidden_layer_sizes=(30, 20),activation="relu",solver='adam',alpha=0.0001,batch_size='auto',learning_rate="adaptive",learning_rate_init=0.0003,\
-    power_t=0.5, max_iter=5, shuffle=True, random_state=None, tol=1e-4, verbose=True, warm_start=True, momentum=0.9, nesterovs_momentum=True, \
+
+mlp = MLPClassifier(hidden_layer_sizes=(30, 15),activation="relu",solver='adam',alpha=0.0001,batch_size='auto',learning_rate="constant",learning_rate_init=0.001,\
+    power_t=0.5, max_iter=4, shuffle=True, random_state=None, tol=1e-4, verbose=True, warm_start=True, momentum=0.9, nesterovs_momentum=True, \
     early_stopping=False, validation_fraction=0.1, beta_1=0.9, beta_2=0.999, epsilon=1e-8)
-mlp.fit(X_train, y_train)
-print("mlp trained")
-
-# mnb_score = mnb_ovr.score(X_test, y_test)
-mlp_score = mlp.score(X_test, y_test)
-# print('mnb:', mnb_score)
-print('mlp score:', mlp_score)
-
-# mlp = MLPClassifier(hidden_layer_sizes=(30, 15),activation="relu",solver='adam',alpha=0.0001,batch_size='auto',learning_rate="adaptive",learning_rate_init=0.001,\
-#     power_t=0.5, max_iter=50, shuffle=True, random_state=None, tol=1e-4, verbose=False, warm_start=True, momentum=0.9, nesterovs_momentum=True, \
-#     early_stopping=False, validation_fraction=0.1, beta_1=0.9, beta_2=0.999, epsilon=1e-8)
-# mlp.fit(X_train, y_train)
-# print("mlp50 trained")
-
-# mlp_score = mlp.score(X_test, y_test)
-# print('mlp 50 iter:', mlp_score)
+mlp.fit(X, y)
+print(mlp.score(X, y))
 
 
-# mlp = MLPClassifier(hidden_layer_sizes=(30, 15),activation="relu",solver='adam',alpha=0.0001,batch_size='auto',learning_rate="adaptive",learning_rate_init=0.001,\
-#     power_t=0.5, max_iter=100, shuffle=True, random_state=None, tol=1e-4, verbose=False, warm_start=True, momentum=0.9, nesterovs_momentum=True, \
-#     early_stopping=False, validation_fraction=0.1, beta_1=0.9, beta_2=0.999, epsilon=1e-8)
-# mlp.fit(X_train, y_train)
-# print("mlp50 trained")
+kaggle_results = mlp.predict_proba(X_kaggle)
+print(kaggle_results)
 
-# mlp_score = mlp.score(X_test, y_test)
-# print('mlp 100 iter:', mlp_score)
-
-# mlp = MLPClassifier(hidden_layer_sizes=(30, 15),activation="relu",solver='adam',alpha=0.0001,batch_size='auto',learning_rate="adaptive",learning_rate_init=0.001,\
-#     power_t=0.5, max_iter=200, shuffle=True, random_state=None, tol=1e-4, verbose=False, warm_start=True, momentum=0.9, nesterovs_momentum=True, \
-#     early_stopping=False, validation_fraction=0.1, beta_1=0.9, beta_2=0.999, epsilon=1e-8)
-# mlp.fit(X_train, y_train)
-# print("mlp50 trained")
-
-# mlp_score = mlp.score(X_test, y_test)
-# print('mlp 200 iter:', mlp_score)
+ids = kaggle_test_df['id']
+predict_data = pd.DataFrame(kaggle_results,  columns=['EAP', 'HPL', 'MWS'])
+conc = pd.concat([ids, predict_data], axis=1)
+conc.to_csv('mySubmission.csv', index=False)
+'''
